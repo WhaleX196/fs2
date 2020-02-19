4bc0e12807f018f13d5bb6534a7da5a819b68139
WhaleX196 <semenishev196@gmail.com>
2020-02-19 13:22:01 +0500

    Output information about disks

--- fs_console/fs_console/fs_console.cpp
+++ fs_console/fs_console/fs_console.cpp
@@ -1,22 +1,147 @@
 // fs_console.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
 #include "Header.h"
+
+
+#include <sstream>
+#include <string.h>
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+using namespace std;
+
+#pragma pack(push, 1)
+
+typedef struct _BOOT_NTFS
+{
+    BYTE    jump[3];
+    BYTE    name[8];
+    UINT16  sec_size;
+    UINT16  secs_cluster;
+    BYTE    zeros_0[6];
+    UINT16  media_desc;
+    BYTE    zeros_1;
+    UINT16  secs_track;
+    UINT16  num_heads;
+    BYTE    zeros_2[8];
+    UINT32  defaultValue;
+    UINT64  num_secs;
+    UINT64  LCNofMFT;
+    UINT64  LCNofMFTMirr;
+    DWORD  clustersPerMFT;
+    UINT32  clustersPerIndex;
+    UINT64  volumeSerialNumber;
+} BOOT_NTFS;
+
+#pragma pack(pop)
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n\n";
 
     
     if (Check_DLL())
     {
-        printf("DLL here!");
+        printf("DLL here!\n\n");
     }
     else
     {
-        printf("Huston, we have a problem!\n");
+        printf("Huston, we have a problem!\n\n");
     }
+
+    DWORD dr = GetLogicalDrives(); // функция возвращает битовую маску
+    //printf("%d\n\n", dr);
+
+    int n;
+
+    for (int x = 0; x < 26; x++) // проходимся циклом по битам
+    {
+        n = ((dr >> x) & 1); // узнаём значение текущего бита
+        if (n) // если единица - диск с номером x есть
+        {
+           // std::string dl = (char)(65 + x); // получаем литеру диска
+            std::string path = std::to_string(65 + x) + ":\\";
+
+            //std::string out = "Диск " + dl + "\n";
+
+            std::cout << "Disk: " << char(65 + x) << "\n" << std::endl;
+        }
+    }
+
+    /*const WCHAR* fileName = L"\\\\.\\F:";
+
+    HANDLE fileHandle = CreateFileW(
+        fileName,
+        GENERIC_READ,
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if (fileHandle == INVALID_HANDLE_VALUE)
+    {
+        perror("Error: ");
+    }
+
+    LARGE_INTEGER sectorOffset;
+    sectorOffset.QuadPart = 0;
+
+    unsigned long currentPosition = SetFilePointer(
+        fileHandle,
+        sectorOffset.LowPart,
+        &sectorOffset.HighPart,
+        FILE_BEGIN
+    );
+
+    if (currentPosition != sectorOffset.LowPart)
+    {
+        perror("Error: ");
+    }
+
+    BYTE dataBuffer[1024];
+
+    DWORD bytesRead;
+
+    bool readResult = ReadFile(
+        fileHandle,
+        dataBuffer,
+        1024,
+        &bytesRead,
+        NULL
+    );
+
+    if (!readResult || bytesRead != 1024)
+    {
+        perror("Error: ");
+    };
+
+
+
+    BOOT_NTFS* pBootRecord = reinterpret_cast<BOOT_NTFS*>(dataBuffer);
+
+    cout << "File system type: " << pBootRecord->name << endl;
+    cout << "Sector size: " << pBootRecord->sec_size << endl;
+    cout << "Sectors per cluster: " << pBootRecord->secs_cluster << endl;
+    cout << "Volume descriptor: " << pBootRecord->media_desc << endl;
+    cout << "Sectors per track: " << pBootRecord->secs_track << endl;
+    cout << "Number of heads: " << pBootRecord->num_heads << endl;
+    cout << "Number of sectors: " << pBootRecord->num_secs << endl;
+    cout << "First cluster of MFT: " << pBootRecord->LCNofMFT << endl;
+    cout << "First cluster of MFT mirror: " << pBootRecord->LCNofMFTMirr << endl;
+    cout << "Clusters per MFT Record: " << pBootRecord->clustersPerMFT << endl;
+    cout << "Clusters per Index Record: " << pBootRecord->clustersPerIndex << endl;
+    cout << "Volume serial number: " << pBootRecord->volumeSerialNumber << endl;
+
+    CloseHandle(fileHandle);*/
+
+    return 0;
+
+
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
